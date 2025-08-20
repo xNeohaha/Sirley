1115bea875d4e0dd81de20159c7223a98c3e6270
xNeohaha <tuberanthony@gmail.com>
2025-08-20 15:12:46 -0300

    Update SeisP.java

--- LivroSirley/src/Atividade/SeisP.java
+++ LivroSirley/src/Atividade/SeisP.java
@@ -1,1 +1,19 @@
+package Atividade;
+
+import java.util.Scanner;
+
+public class SeisM {
+      public static void main(String[] args) {
+        System.out.println("Programa para calcular o novo salário com reajuste.");
+        Scanner entrada = new Scanner(System.in);
+        double sm, pr, ns;      
+        System.out.println("Digite o salário mensal atual: ");
+        sm = entrada.nextDouble();       
+        System.out.println("Digite o percentual de reajuste (%): ");
+        pr = entrada.nextDouble();
+        ns = sm + (sm * pr / 100);
+        System.out.println("O novo salário é: R$ " + ns);
+        entrada.close();
+    }
+}
 
