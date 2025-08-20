dba3c75046e531e7759089533dbfce43e8b909c0
xNeohaha <tuberanthony@gmail.com>
2025-08-20 08:27:12 -0300

    Update SeisN.java

--- LivroSirley/src/Atividade/SeisN.java
+++ LivroSirley/src/Atividade/SeisN.java
@@ -6,9 +6,9 @@ public class SeisN {
     public static void main(String[] args) {
         System.out.println("Programa para Calcular o volume de uma caixa triangular.");
         Scanner entrada = new Scanner(System.in);
-        double cm, lar, alt, vol;
+        int a,b,c,res;
         System.out.println("Digite o comprimento da caixa: ");
-        cm = entrada.nextDouble();
+        cm = entrada.nextInt();
         System.out.println("Digite a largura da caixa: ");
         lar = entrada.nextDouble();
         System.out.println("Digite a altura da caixa: ");
