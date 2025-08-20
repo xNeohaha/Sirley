cbc38cc04bbf3ba5193fb1d0a75fc45778e0df7f
xNeohaha <tuberanthony@gmail.com>
2025-08-20 08:46:52 -0300

    Update SeisO.java

--- LivroSirley/src/Atividade/SeisO.java
+++ LivroSirley/src/Atividade/SeisO.java
@@ -1,1 +1,26 @@
+package Atividade;
+
+import java.util.Scanner;
+
+public class SeisO {
+    public static void main(String[] args) {
+        System.out.println("Programa para A + B + C, resultad no valor quadrado da soma dos três valores.");
+        Scanner entrada = new Scanner(System.in);
+        int a,b,c,d,p,s;
+        System.out.println("Digite o valor de A: ");
+        a = entrada.nextInt();
+        System.out.println("Digite o valor de B: ");
+        b = entrada.nextInt();
+        System.out.println("Digite o valor de C: ");
+        c = entrada.nextInt();
+        System.out.println("Digite o valor de D: ");
+        d = entrada.nextInt();
+        p = (a * c);
+        s = (b + d);
+        
+        System.out.println("O resultado P é: " + p);
+        System.out.println("O resultado P é: " + s);
+        entrada.close();
+    }
+}
 
