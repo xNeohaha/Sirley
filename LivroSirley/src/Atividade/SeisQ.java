cd62600babefee862827a97022cee41eb42768f4
xNeohaha <tuberanthony@gmail.com>
2025-08-20 15:15:08 -0300

    Update SeisQ.java

--- LivroSirley/src/Atividade/SeisQ.java
+++ LivroSirley/src/Atividade/SeisQ.java
@@ -1,1 +1,26 @@
+package Atividade;
+
+import java.util.Scanner;
+
+public class SeisQ {
+        public static void main(String[] args) {
+        System.out.println("Programa para calcular a soma dos quadrados de três números inteiros.");
+        Scanner entrada = new Scanner(System.in);
+        int a, b, c, soma;
+        
+        System.out.println("Digite o valor de A: ");
+        a = entrada.nextInt();
+        
+        System.out.println("Digite o valor de B: ");
+        b = entrada.nextInt();
+        
+        System.out.println("Digite o valor de C: ");
+        c = entrada.nextInt();
+        
+        soma = (a * a) + (b * b) + (c * c);
+        
+        System.out.println("A soma dos quadrados é: " + soma);
+        entrada.close();
+    }
+}
 
